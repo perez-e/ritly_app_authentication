bce9550cfb19dac4719c388803cd5c98628c486d
Eric Perez <ericricardoperez@gmail.com>
2014-01-27 16:07:30 -0800

    annotate

--- app/models/ritly.rb
+++ app/models/ritly.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: ritlies
+#
+#  id            :integer          not null, primary key
+#  link          :string(255)
+#  random_string :string(255)
+#  visits        :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class Ritly < ActiveRecord::Base
 end
