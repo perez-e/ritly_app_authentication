7e5f9789ed5ebd01963d84f039f8b8a762f356f8
Eric Perez <ericricardoperez@gmail.com>
2014-01-27 21:15:12 -0800

    inlcude sessions module into application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,18 @@
 #  password_digest :string(255)
 #  created_at      :datetime
 #  updated_at      :datetime
+#  remember_token  :string(255)
 #
 
 class User < ActiveRecord::Base
-	has_secure_password          #make password and password_confirmation attributes to User object 
+	has_secure_password   
+
+	before_save { |user| user.email = email.downcase }
+	before_save :create_remember_token  
+
+	private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end  
 end
