a8db5ae9c30b9265877523a3dec2a3b9312a200a
Eric Perez <ericricardoperez@gmail.com>
2014-01-27 17:23:10 -0800

    users controller: show, new, create. appropriate views and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+	def new
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def create 
+		user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		user = User.new(user_params)
+
+		if user.save
+	      flash[:notice] = "Welcome to the Ritly app!"
+	      # sign_in user
+	      redirect_to action: :show, id: user.id
+	    else
+	   	  flash[:notice] = "You are not created!!"
+	      render'new'
+	    end
+
+	end
 end
