b60aa7a3335f06561752d2df33246bc0d37cf9b2
Eric Perez <ericricardoperez@gmail.com>
2014-01-27 16:08:06 -0800

    schema: users and ritly

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124190821) do
+ActiveRecord::Schema.define(version: 20140127235914) do
 
   create_table "ritlies", force: true do |t|
     t.string   "link"
     t.string   "random_string"
     t.integer  "visits"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
