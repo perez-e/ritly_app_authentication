468e85bca39a8dc00067770bc625c98960c16ab8
Eric Perez <ericricardoperez@gmail.com>
2014-01-27 21:14:07 -0800

    remember token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127235914) do
+ActiveRecord::Schema.define(version: 20140128051020) do
 
   create_table "ritlies", force: true do |t|
     t.string   "link"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20140127235914) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
